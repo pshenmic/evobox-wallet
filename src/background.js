4a892e24f5ba4b460d11a3ac26de8da027afbdbc
pshenmic <pshenmic@gmail.com>
2024-03-21 18:58:08 +0300

    Add check for execution environment

--- src/background.js
+++ src/background.js
@@ -1,20 +1,22 @@
-// // import lodash from 'lodash';
-// // import DapiClient from '.././../platform/packages/js-dapi-client/dist/dapi-client.min'
-// chrome.runtime.onMessage.addListener(handleMessages);
-//
-// async function handleMessages(message) {
-//     // if (running) {
-//     //     console.log('request is still loading')
-//     //     return
-//     // }
-//     switch (message) {
-//         case 'test': {
-//             console.log('message from test', message)
-//
-//         }
-//         default:
-//             console.warn('Unknown message: ' + message)
-//     }
-// }
-//
-// console.log('background script loading finished')
+import {getRunningEnv} from "./getRunningEnv";
+import ExecutingEnvironment from "./constants";
+import React from "react";
+
+if (getRunningEnv() === ExecutingEnvironment.CONTENT) {
+    chrome.runtime.onMessage.addListener(handleMessages);
+
+    async function handleMessages(message) {
+        switch (message) {
+            case 'test': {
+                console.log('message from test', message)
+                break
+            }
+            default:
+                console.warn('Unknown message: ' + message)
+        }
+    }
+
+    console.log('background script loading finished')
+}
+
+
--- src/content-script.js
+++ src/content-script.js
@@ -2,30 +2,36 @@ import "./ui/styles/app.css";
 import App from "./ui/app";
 import ReactDOM from 'react-dom/client';
 import React from 'react';
+import ExecutingEnvironment from "./constants";
+import {getRunningEnv} from "./getRunningEnv";
 
-const root = document.createElement("div")
-root.className = "container"
-document.body.appendChild(root)
+console.log(getRunningEnv())
 
-const rootDiv = ReactDOM.createRoot(root);
+if(getRunningEnv() === ExecutingEnvironment.CONTENT) {
 
-rootDiv.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
-);
+    const root = document.createElement("div")
+    root.className = "container"
+    document.body.appendChild(root)
+
+    const rootDiv = ReactDOM.createRoot(root);
+
+    rootDiv.render(
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>
+    );
 
 
-//
-// console.log('content script loaded')
-//
-// window.addEventListener("message",
-//     async function (request, sender, sendResponse) {
-//         console.log(request, sender, sendResponse)
-//         if (chrome.runtime) {
-//             chrome.runtime.sendMessage('test', (response) => {
-//                 console.log('received user data', response);
-//             });
-//         }
-//     }
-// );
+    console.log('content script loaded')
+
+    window.addEventListener("message",
+        async function (request, sender, sendResponse) {
+            console.log(request, sender, sendResponse)
+            if (chrome.runtime) {
+                chrome.runtime.sendMessage('test', (response) => {
+                    console.log('received user data', response);
+                });
+            }
+        }
+    );
+}
